8ec93eca8dc6556301a617ea26fba6520f964b2c
Elsi Cervanaku <elsi@Elsi.>
2025-10-05 19:18:54 +0200

    not last for sure

--- data/cards.js
+++ data/cards.js
@@ -1,8 +1,8 @@
 const allCards = [
-    new Card("Irrigation", { addRain: 30 }, "/assets/irrigation.png"),
-    new Card("Greenhouse", { tempBuffer: 5 , }, "/assets/greenhouse.png"),
-    new Card("Fertilizer", { flatGrowthPercent: 10 }, "/assets/fertilizer.png"),
-    new Card("Shade Net", { tempBuffer: -5 }, "/assets/shade_net.png")
+    new Card("Irrigation", { addRain: 30 }, "../assets/irrigation.png"),
+    new Card("Greenhouse", { tempBuffer: 5 , }, "../assets/greenhouse.png"),
+    new Card("Fertilizer", { flatGrowthPercent: 10 }, "../assets/fertilizer.png"),
+    new Card("Shade Net", { tempBuffer: -5 }, "../assets/shade_net.png")
 ];
 
 
